242b80b1228bbc94891d3f08405104cd58d345c1
Auburg <tanvirkazi@hotmail.com>
2026-01-04 17:12:14 +0000

    rect  lighting

--- src/scenes/porsche.js
+++ src/scenes/porsche.js
@@ -1,11 +1,23 @@
 import * as THREE from 'three';
+import {RectAreaLightUniformsLib} from 'three/addons/lights/RectAreaLightUniformsLib.js';
+import {RectAreaLightHelper} from 'three/addons/helpers/RectAreaLightHelper.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import GUI from 'lil-gui'; 
 import { colorGUIHelper } from '../utils/color_gui_helper.js';
+import { degRadHelper } from '../utils/deg_rad_helper.js';
+
+function makeXYZGUI(gui, vector3, name, onChangeFn) {
+  const folder = gui.addFolder(name);
+  folder.add(vector3, 'x', -10, 10).onChange(onChangeFn);
+  folder.add(vector3, 'y', 0, 10).onChange(onChangeFn);
+  folder.add(vector3, 'z', -10, 10).onChange(onChangeFn);
+  folder.open();
+}
 
 const canvas = document.querySelector('#c');
 const renderer = new THREE.WebGLRenderer({ canvas });
+RectAreaLightUniformsLib.init();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 10, 20);
@@ -17,7 +29,6 @@ scene.background = new THREE.Color(0x333333); // Gray background so we know rend
 const controls = new OrbitControls(camera, canvas);
 controls.target.set(0, 5, 0);
 controls.update();
-
 
 const loader = new GLTFLoader();
 
@@ -33,17 +44,30 @@ loader.load('/assets/2023_porsche_911_gt3_rs_2.7_carrera_tribute_992.glb', funct
   console.error('Error loading model:', error);
 });
 
-// Add stronger lighting
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.0);
-scene.add(ambientLight);
 
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(10, 10, 10);
+/* const ambientLight = new THREE.AmbientLight(0xffffff, 1.0);
+scene.add(ambientLight); */
+
+const color = 0xFFFFFF;
+const intensity = 150;
+const width = 12;
+const height = 4;
+const light = new THREE.RectAreaLight(color, intensity, width, height);
+light.position.set(0, 10, 0);
+light.rotation.x = THREE.MathUtils.degToRad(-90);
+//light.target.position.set(0, 0, 0);
+//scene.add(light);
 scene.add(light);
 
-const light2 = new THREE.DirectionalLight(0xffffff, 0.5);
-light2.position.set(-10, 10, -10);
-scene.add(light2);
+const helper = new RectAreaLightHelper(light);
+scene.add(helper);
+
+/*function updateLight() {
+  //light.target.updateMatrixWorld();
+  helper.update();
+}
+ 
+updateLight();*/
 
 const planeSize = 40;
   
@@ -67,7 +91,17 @@ scene.add(mesh);
 
 const gui = new GUI();
 gui.addColor(new colorGUIHelper(light, 'color'), 'value').name('color');
-gui.add(light, 'intensity', 0, 5, 0.01);
+gui.add(light, 'intensity', 0, 10, 0.01);
+gui.add(light, 'width', 0, 20);
+gui.add(light, 'height', 0, 20);
+gui.add(new degRadHelper(light.rotation, 'x'), 'value', -180, 180).name('x rotation');
+gui.add(new degRadHelper(light.rotation, 'y'), 'value', -180, 180).name('y rotation');
+gui.add(new degRadHelper(light.rotation, 'z'), 'value', -180, 180).name('z rotation');
+ 
+makeXYZGUI(gui, light.position, 'position');
+
+//makeXYZGUI(gui, light.position, 'position', updateLight);
+//makeXYZGUI(gui, light.target.position, 'target', updateLight);
 
 // Animation loop to render the scene
 function animate() {
---
+++ src/utils/deg_rad_helper.js
@@ -0,0 +1,13 @@
+import * as THREE from 'three';
+export class degRadHelper {
+  constructor(obj, prop) {
+    this.obj = obj;
+    this.prop = prop;
+  }
+  get value() {
+    return THREE.MathUtils.radToDeg(this.obj[this.prop]);
+  }
+  set value(v) {
+    this.obj[this.prop] = THREE.MathUtils.degToRad(v);
+  }
+}
